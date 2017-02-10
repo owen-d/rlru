377d47fc514dce6f46f066cb5d9f26b462100c06
Owen Diehl <ow.diehl@gmail.com>
2017-02-10 18:31:01 -0500

    begins set method

--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,56 @@
 //has
 use std::collections::HashMap;
 
+// Doubly linked list
 struct Node <V> {
-    prev: Box<Option<Node<V>>>,
-    next: Box<Option<Node<V>>>,
+    prev: Option<Box<Node<V>>>,
+    next: Option<Box<Node<V>>>,
     val: V,
 }
 
 pub struct LRU <K, V> {
     cache: HashMap<K, V>,
-    head: Node<V>,
-    tail: Node<V>,
+    head: Option<Node<V>>,
+    tail: Option<Node<V>>,
     len: u16,
+}
+
+impl <K: Eq + std::hash::Hash, V> LRU <K, V> {
+    pub fn new() -> LRU<K, V> {
+        LRU {
+            cache: HashMap::new(),
+            head: None,
+            tail: None,
+            len: 0
+        }
+    }
+    pub fn set(&mut self, key: K, val: V) -> &mut LRU<K, V>{
+        use std::collections::hash_map::Entry;
+        if let Some(node) = self.cache.get(&key) {
+            // remove node from linked list, and replace at head
+            self
+        } else {
+            let addition = Node {
+                prev: None,
+                // wrap it in a layer of indirection if exists.
+                next: self.head.map(|x| Box::new(x)),
+                val: val
+            };
+            self.head = Some(addition);
+            self
+        }
+    }
 }
 
 
 #[cfg(test)]
 mod tests {
+    use super::LRU;
+
     #[test]
-    fn it_works() {}
+    fn lru_instantiaion() {
+        let lru: LRU<&str, u8> = LRU::new();
+    }
+
+    // #[test]
 }
