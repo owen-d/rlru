5d098cd67527ad7cd50038ef6623d8c19e50e73a
owen <ow.diehl@gmail.com>
2017-02-27 23:36:27 -0500

    implements push head/tail for doubly_linked

--- src/double_link.rs
+++ src/double_link.rs
@@ -31,4 +31,39 @@ impl<T> DoubleLink<T> {
             tail: None,
         }
     }
+
+    fn push_head(&mut self, elem: T) -> &mut Self {
+        let new_node = Node::new(elem);
+        match self.head.take() {
+            Some(prev_head) => {
+                // set new_node's next prop to prev head
+                new_node.borrow_mut().next = Some(prev_head);
+                // set prev head's prev prop to new node
+                new_node.borrow_mut().prev = Some(new_node.clone());
+                // set self's head to new node
+                self.head = Some(new_node);
+            },
+            None => {
+                self.head = Some(new_node.clone());
+                self.tail = Some(new_node);
+            }
+        };
+        self
+    }
+
+    fn push_tail(&mut self, elem: T) -> &mut Self {
+        let new_node = Node::new(elem);
+        match self.tail.take() {
+            Some(prev_tail) => {
+                prev_tail.borrow_mut().next = Some(new_node.clone());
+                new_node.borrow_mut().prev = Some(prev_tail);
+                self.tail = Some(new_node);
+            },
+            None => {
+                self.head = Some(new_node.clone());
+                self.tail = Some(new_node);
+            }
+        };
+        self
+    }
 }
