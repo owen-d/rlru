8b7832868d40f9f30f53320f3a529a66edd7b238
owen <ow.diehl@gmail.com>
2017-03-09 15:25:35 -0500

    internal list splicing

--- src/rlru.rs
+++ src/rlru.rs
@@ -2,12 +2,12 @@ use std::collections::HashMap;
 use std::collections::hash_map::Entry;
 use std::hash::Hash;
 use std::rc::Rc;
-// use std::cell::RefCell;
+use std::cell::RefCell;
 
 struct Rlru<K, V>
     where K: Eq + Hash
 {
-    cache: HashMap<K, Rc<Node<V>>>,
+    cache: HashMap<K, Rc<RefCell<Node<V>>>>,
     head: Link<V>,
     tail: Link<V>,
     max_length: u32,
@@ -30,7 +30,7 @@ impl<T> Node<T> {
     }
 }
 
-type Link<T> = Option<Rc<Node<T>>>;
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
 
 
 
@@ -55,16 +55,25 @@ impl<K, V> Rlru<K, V>
         }
     }
 
-    fn splice_node(&mut self, node: Node<V>) {
+    fn splice_node(&mut self, node: Rc<RefCell<Node<V>>>) {
         // update node's prev/next pointers to link to each other
+        {
+            let node = node.borrow_mut();
+            node.prev.map(|prev_node| {
+                prev_node.borrow_mut().next = node.next.clone();
+            });
+            node.next.take().map(|next_node| {
+                next_node.borrow_mut().prev = node.prev.take();
+            });
+        }
         // insert node at head of rlru.
+        self.push(node);
     }
 
-    fn push(&mut self, node: Node<V>) -> &mut Self {
-        let node = Rc::new(node);
+    fn push(&mut self, node: Rc<RefCell<Node<V>>>) -> &mut Self {
         self.head.take().map(|prev_head| {
-            prev_head.prev = Some(node.clone());
-            node.next = Some(prev_head);
+            prev_head.borrow_mut().prev = Some(node.clone());
+            node.borrow_mut().next = Some(prev_head);
         });
         self.head = Some(node.clone());
         self
@@ -72,18 +81,21 @@ impl<K, V> Rlru<K, V>
 
     fn pop(&mut self) {
         self.tail.take().map(|node| {
-            if node.prev.is_none() {
+            if node.borrow_mut().prev.is_none() {
                 // have to worry about updating head.
                 self.head.take();
             }
-            self.tail = node.prev;
+            self.tail = node.borrow_mut().prev;
         });
     }
 
     pub fn get(&mut self, key: K) -> Option<&V> {
         match self.cache.entry(key) {
-            Entry::Occupied(node) => {
+            Entry::Occupied(entry) => {
                 // splice node to front
+                let found = entry.get();
+                self.splice_node(found.clone());
+                Some(&found.as_ref().borrow().elem)
             },
             Entry::Vacant(_) => None
         }
